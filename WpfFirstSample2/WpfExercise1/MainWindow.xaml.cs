22d4ed8a4fbbe7d2dca1080db1d9af1209487272
hindrekhan <hindrek.hannus@tptlive.ee>
2018-03-06 09:48:21 +0200

    a

---
+++ WpfFirstSample2/WpfExercise1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExercise1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Human> items = new List<Human>();
+            items.Add(new Human() { FirstName = "Elmo", LastName = "Kelmo", Age = 25 });
+            items.Add(new Human() { FirstName = "Juhan", LastName = "Aabits", Age = 34 });
+            items.Add(new Human() { FirstName = "Mari", LastName = "Maasikas", Age = 21 });
+
+
+
+            TodoListBox.ItemsSource = items;
+        }
+    }
+
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WpfFirstSample2/WpfList/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] allLines = System.IO.File.ReadAllLines("../../names.txt");
+            List<TodoItem> todo = new List<TodoItem>();
+
+            foreach(string line in allLines)
+            {
+                TodoItem item = new TodoItem();
+
+                item.Title = line;
+                item.Completion = 5;
+
+                todo.Add(item);
+            }
+
+            TodoListBox.ItemsSource = todo;            
+        }
+
+        private void todoListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(TodoListBox.SelectedItem != null)
+            {
+                Title = (TodoListBox.SelectedItem as TodoItem).Title;
+            }
+        }
+
+        private void btnShowSelectedItem_Click(object sender, RoutedEventArgs e)
+        {            
+            foreach (var item in TodoListBox.SelectedItems)
+            {
+                MessageBox.Show((item as TodoItem).Title);
+
+            }
+        }
+
+        private void btnSelectLast_Click(object sender, RoutedEventArgs e)
+        {
+            TodoListBox.SelectedIndex = TodoListBox.Items.Count - 1;
+        }
+
+        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
+        {
+            int nextIndex = 0;
+            if((TodoListBox.SelectedIndex >= 0) && (TodoListBox.SelectedIndex < (TodoListBox.Items.Count - 1)))
+            {
+                nextIndex = TodoListBox.SelectedIndex + 1;
+            }
+            TodoListBox.SelectedIndex = nextIndex;
+        }
+
+        private void btnSelectCSharp_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                if((item is TodoItem) && ((item as TodoItem).Title.Contains("C#")))
+                {
+                    TodoListBox.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void btnSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                TodoListBox.SelectedItems.Add(item);
+            }
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+}
